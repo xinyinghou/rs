4c770a909e1ef365463113bbf52113d9ce0f6c3a
Zihan Wu <ziwu@m-c02cc15hlvdn.attlocal.net>
2024-01-19 14:30:55 -0500

    Fixing import for xinying backend

--- bases/rsptx/book_server_api/routers/personalized_parsons/evaluate_fixed_code.py
+++ bases/rsptx/book_server_api/routers/personalized_parsons/evaluate_fixed_code.py
@@ -6,7 +6,7 @@ from fuzzywuzzy import fuzz
 from types import ModuleType
 import difflib
 import signal
-from personalize_common_solution import * 
+from .personalize_common_solution import * 
 
 class NullOutput:
     def write(self, _):
